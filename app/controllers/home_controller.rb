5bcd65a23edefcb1f9b8ce1ebdd2096d8c981a6f
kkgarg <kamalkgarg@hotmail.com>
2016-10-14 08:21:23 +0000

    jwt authentication working good with devise and jwt

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
+
+  before_filter :authenticate_request!  
+
   def index
+    render json: {logged_in: true}
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
+  post 'auth_user' => 'authentication#authenticate_user'
   get 'sandbox', to: 'sandbox#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
