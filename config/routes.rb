dbeee0d4e79235131295f90db9961071aa2d7ae1
kkgarg <kamalkgarg@hotmail.com>
2016-10-16 02:38:27 +0000

    Basic authentication working

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
 
   namespace :api, defaults: { format: :json}  do 
+    post '/sign_in', to: 'session#create'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
