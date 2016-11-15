7ca6e3d4b3ab67923c28116a925a45e199707c93
kkgarg <kamalkgarg@hotmail.com>
2016-11-15 05:45:29 +0000

    setup relations between todo models

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  belongs_to :list, touch: true
+  delegate :user, to: :list
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :lists
+  has_many :tasks, through: :lists
 end
